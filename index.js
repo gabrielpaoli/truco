b78a5d99abad104e2cd60121df23358138b8da3f
gabriel.paoli <gabriel.paoli@mediamonks.com>
2019-09-15 22:09:43 -0300

    add counter

--- index.js
+++ index.js
@@ -18,26 +18,35 @@ http.listen(3000, function(){
   console.log('listening on *:3000');
 });
 
-var hand = 1;
+var hand = 0;
 var points = 0;
+var counter = 0;
 
 io.on('connection', function(socket){
+
   socket.on('init', function(msg){
     var game = new Game(hand);
-    
     hand = whoIsHand(hand);
     io.emit('init', game.createCardsPerPlayer());
 
     socket.on('card', function(msg){
-      console.log(msg);
+      console.log(counter);
+      counter = incrementCounter(counter);
+//      console.log(counter);
       io.emit('card', msg);
     });
-
   });
 
-
-
 });
+
+function incrementCounter(counter){
+  if(counter === 5){
+    counter = 0;
+  }else{
+    counter++;
+  }
+  return counter;
+}
 
 function whoIsHand(hand){
   if(hand === 0){
